21d4f83cbfb6f8b7bea527faa2e0e527db5b9be9
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-06 01:46:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
     {  return "I can\'t hear you!"
     }
     else if
-    {return "YES INDEED!"
+    return "YES INDEED!"
     }
 
     else if (string.toUpperCase)
