55c61f9e8fe792674b54ba2fb93dd5b14b0062de
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-06 01:45:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase)
+    if (string.toLowerCase)
 
-  {  return "I can\'t hear you!"
-  }
-  else if
-  {  return "YES INDEED!"
-  }
+    {  return "I can\'t hear you!"
+    }
+    else if
+    {return "YES INDEED!"
+    }
 
-  else if (string.toUpperCase)
+    else if (string.toUpperCase)
 
     {return "YES INDEED!"
-  }
-  else (string === "I love you, Grandma.")
-  {
-    return "I love you, too."
-  }
+    }
+    else (string === "I love you, Grandma.")
+
+    {return "I love you, too."
+    }
 
 }
